e231952a84611346b8f77c073ffd853d041ca629
Aliaksei Zabkou <aliaksei.zabkou@gmail.com>
2020-12-08 20:23:22 +0300

    Add functionality to Main()

--- LinqWithEFCore/Program.cs
+++ LinqWithEFCore/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using static System.Console;
+using Packt.CS7;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace LinqWithEFCore
 {
@@ -6,7 +9,18 @@ namespace LinqWithEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new Northwind())
+            {
+                var query = db.Products
+                    .Where(product => product.UnitPrice < 10M)
+                    .OrderByDescending(product => product.UnitPrice);
+                WriteLine("Products that cost less than $10:");
+                foreach (var item in query)
+                {
+                    WriteLine($"{item.ProductID}: {item.ProductName} costs {item.UnitPrice:$#,##0.00}");
+                }
+                WriteLine();
+            }
         }
     }
 }
