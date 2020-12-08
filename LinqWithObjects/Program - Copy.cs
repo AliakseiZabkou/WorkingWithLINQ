2433824acb190a1a3b00d58ffe8e14ba086df896
Aliaksei Zabkou <aliaksei.zabkou@gmail.com>
2020-12-08 18:16:03 +0300

    Add project files.

---
+++ LinqWithObjects/Program - Copy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
