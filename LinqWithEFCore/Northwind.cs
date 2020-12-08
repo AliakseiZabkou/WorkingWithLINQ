2caec2cbe469618ae4dfcb7a2b2929d003e8ece6
Aliaksei Zabkou <aliaksei.zabkou@gmail.com>
2020-12-08 20:17:15 +0300

    adding Context

---
+++ LinqWithEFCore/Northwind.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Packt.CS7
+{
+    // управление подключением к БД
+    public class Northwind : DbContext
+    {
+        // эти свойства соотносятся с таблицами в базе
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+        {
+             optionsBuilder.UseSqlServer(
+                 @"Data Source=(localdb)\mssqllocaldb;" +
+                 "Initial Catalog=Northwind;" +
+                 "Integrated Security=true;" +
+                 "MultipleActiveResultSets=true;");
+        }
+    }
+}
