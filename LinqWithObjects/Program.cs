556680cb81f6e637e3e06b42d580ad1acd3508e2
Aliaksei Zabkou <aliaksei.zabkou@gmail.com>
2020-12-08 19:54:49 +0300

    Simplify 'LinqWithArrayOfStrings()' method

--- LinqWithObjects/Program.cs
+++ LinqWithObjects/Program.cs
@@ -14,16 +14,13 @@ namespace LinqWithObjects
         static void LinqWithArrayOfStrings()
         {
             var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
-            var query = names.Where(new Func<string, bool>(NameLongerThanFour));  
+            var query = names.Where(name => name.Length > 4)
+                .OrderBy(name => name.Length).ThenBy(name => name);  
             foreach(string item in query)
             {
                 WriteLine(item);
             }
         }
 
-        static bool NameLongerThanFour(string name)
-        {
-            return name.Length > 4;
-        }
     }
 }
